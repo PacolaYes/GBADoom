b01e8ce77b71589609138af3f2fa2fe6089c1530
Pacola <149804664+PacolaYes@users.noreply.github.com>
2025-04-19 20:55:37 -0300

    change iwad to srb2 xmas

--- source/doom_iwad.c
+++ source/doom_iwad.c
@@ -2,6 +2,6 @@
 #include "doom_iwad.h"
 
 //Uncomment which edition you want to compile
-//#include "iwad/SRB2WEEN.c"
-#include "iwad/srb2.c"
+#include "iwad/srb2xmas.c"
+//#include "iwad/srb2.c"
 const unsigned int doom_iwad_len = sizeof(doom_iwad);
