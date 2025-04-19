31f45a396c33f0256385131ccf2078f58c0348bb
Pacola <149804664+PacolaYes@users.noreply.github.com>
2025-04-19 18:08:34 -0300

    srb2 stuff

--- source/doom_iwad.c
+++ source/doom_iwad.c
@@ -2,11 +2,6 @@
 #include "doom_iwad.h"
 
 //Uncomment which edition you want to compile
-#include "iwad/doom1.c"
-//#include "iwad/doomu.c"
-//#include "iwad/doom2.c"
-//#include "iwad/tnt.c"
-//#include "iwad/plutonia.c"
-//#include "iwad/sigil.c"
-
+//#include "iwad/SRB2WEEN.c"
+#include "iwad/srb2.c"
 const unsigned int doom_iwad_len = sizeof(doom_iwad);
